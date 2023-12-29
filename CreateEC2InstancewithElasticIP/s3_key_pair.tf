475978df99944079de09095bec2842236a198f13
kavirajravalji0410 <124714320+kavirajravalji0410@users.noreply.github.com>
2023-12-29 19:11:59 +0530

    Update s3_key_pair.tf

--- CreateEC2InstancewithElasticIP/s3_key_pair.tf
+++ CreateEC2InstancewithElasticIP/s3_key_pair.tf
@@ -1,5 +1,5 @@
 # Data source to retrieve the key pair from S3
 data "aws_s3_bucket_object" "key_pair_object" {
   bucket = "aws-keypair-terraform" # Replace with your S3 bucket name
-  key    = "terraform_jenkins.pem"          # Replace with the key name in the S3 bucket
+  key    = "terraform_ec2_jenkins.pem"          # Replace with the key name in the S3 bucket
 }
