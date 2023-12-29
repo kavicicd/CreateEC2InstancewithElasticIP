7aca9995da889feda0ec83bf67d41554ea740432
kravalji <kravalji@aakashinformatics.com>
2023-12-29 17:10:28 +0530

    delete old files

--- s3_key_pair.tf
+++
@@ -1,5 +0,0 @@
-# Data source to retrieve the key pair from S3
-data "aws_s3_bucket_object" "key_pair_object" {
-  bucket = "aws-keypair-terraform" # Replace with your S3 bucket name
-  key    = "terraform_jenkins.pem"          # Replace with the key name in the S3 bucket
-}
