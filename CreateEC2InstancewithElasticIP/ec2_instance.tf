bb19d9cf27b25863c6fd55d45b5f34cb60f48906
kavirajravalji0410 <124714320+kavirajravalji0410@users.noreply.github.com>
2024-01-01 12:46:40 +0530

    Update ec2_instance.tf

--- CreateEC2InstancewithElasticIP/ec2_instance.tf
+++ CreateEC2InstancewithElasticIP/ec2_instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2_instance" {
   ami           = "ami-0a5ac53f63249fba0" # Need to change AMI ID as per your aws region, bcoz AMI ID are different in each region
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
-  key_name      = "terraform_jenkins" # Update with the key pair name
+  key_name      = "terraform_ec2_jenkins" # Update with the key pair name
 
   vpc_security_group_ids = [aws_security_group.instance_sg.id]
 
